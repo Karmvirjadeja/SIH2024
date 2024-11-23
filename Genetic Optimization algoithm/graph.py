f45f9109f068fb1bdd9c3c3807a6a392dbd97dbf
Karmvir <jadejakarmvir12@gmail.com>
2024-11-23 08:42:47 +0530

    here added the properties

--- Genetic Optimization algoithm/graph.py
+++ Genetic Optimization algoithm/graph.py
@@ -1,3 +1,4 @@
+import random
 import plotly.graph_objects as go
 
 # Parameters
@@ -6,15 +7,26 @@ node_interval = 111
 num_paths = 5
 levels = total_distance // node_interval
 
-# Generate node positions and edges
+# Generate node positions, edges, and additional properties
 node_positions = {}
+node_properties = {}  # To store properties for each node
 edges = []
 
 # Start node
 node_positions["Start"] = (0, 0)
+node_properties["Start"] = {
+    "distance": 0,
+    "wave_height": 0,
+    "wind_speed": 0,
+}
 
 # End node
 node_positions["End"] = (total_distance, 0)
+node_properties["End"] = {
+    "distance": total_distance,
+    "wave_height": 0,
+    "wind_speed": 0,
+}
 
 # Intermediate nodes
 for path in range(1, num_paths + 1):
@@ -23,6 +35,13 @@ for path in range(1, num_paths + 1):
         x = level * node_interval  # Distance along x-axis
         y = path * 2  # Vertical position based on the path
         node_positions[node_id] = (x, y)
+
+        # Assign random values for wave height and wind speed
+        node_properties[node_id] = {
+            "distance": x,
+            "wave_height": round(random.uniform(0.5, 3.0), 2),  # Random wave height in meters
+            "wind_speed": round(random.uniform(5, 25), 1),  # Random wind speed in knots
+        }
 
         # Connect nodes in the same path
         if level == 1:
@@ -45,6 +64,12 @@ node_x = [pos[0] for pos in node_positions.values()]
 node_y = [pos[1] for pos in node_positions.values()]
 node_labels = list(node_positions.keys())
 
+# Prepare hover text
+hover_texts = [
+    f"Node: {node}<br>Distance: {props['distance']} km<br>Wave Height: {props['wave_height']} m<br>Wind Speed: {props['wind_speed']} knots"
+    for node, props in node_properties.items()
+]
+
 # Create Plotly figure
 fig = go.Figure()
 
@@ -57,7 +82,7 @@ fig.add_trace(go.Scatter(
     hoverinfo='none'
 ))
 
-# Add nodes
+# Add nodes with hover text
 fig.add_trace(go.Scatter(
     x=node_x,
     y=node_y,
@@ -65,6 +90,8 @@ fig.add_trace(go.Scatter(
     text=node_labels,
     textposition="top center",
     marker=dict(size=8, color='blue'),
+    hoverinfo='text',
+    hovertext=hover_texts  # Include hover text
 ))
 
 # Layout adjustments for full-screen view and zooming
