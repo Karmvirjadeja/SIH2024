1a4d0a511a19e78fe4938dd7afb579eb732bedf4
umesh2407 <choudharyumesh986@gmail.com>
2024-12-10 11:23:04 +0530

    added nearest port feauture

--- Web/backend/routes/savelocation.js
+++ Web/backend/routes/savelocation.js
@@ -1,10 +1,13 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 import express from "express";
 import { saveLocation } from "../controller/saveLocation.js";
+import { findNearestPort } from "../controller/saveLocation.js";
+
 
 
 const router = express.Router();
 
 router.post("/savelocation", saveLocation)
+router.post("/nearest-port", findNearestPort);
 
 export default router
