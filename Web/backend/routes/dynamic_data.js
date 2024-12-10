8849660127d7c71c2495f13fbfe3089e2c472ee6
Karmvir <jadejakarmvir12@gmail.com>
2024-12-10 15:48:18 +0530

    Here removed the refresh

--- Web/backend/routes/dynamic_data.js
+++ Web/backend/routes/dynamic_data.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getWeatherData} from "../controller/dynamic_data.js";
-import {  getDataByCoordinates } from "../controller/getAllData.js";
+import { getWeatherData } from "../controller/dynamic_data.js";
+import { getDataByCoordinates } from "../controller/getAllData.js";
 
 const router = Router();
 
-router.get("/data", getWeatherData);
-router.get("/alldata",getDataByCoordinates );
+router.post("/data", getWeatherData);
+router.get("/alldata", getDataByCoordinates);
 export default router;
